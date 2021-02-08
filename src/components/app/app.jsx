5da7a5b21a008154f88d696f8dc588f85770d60e
yura <boets003@mail.ru>
2021-02-08 06:27:58 +0300

    add: links

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
 import SignIn from "../sign-in/sign-in.jsx";
@@ -8,17 +9,44 @@ import AddReview from "../add-review/add-review.jsx";
 import Film from "../film/film.jsx";
 import Error404 from "../error-404/error-404";
 
+
+// {/*<>*/}
+// {/* <Main mainFilms = {mainFilms}/>*/}
+// {/* <MyList myListFilms={myListFilms}/>*/}
+// {/* <Player/>*/}
+// {/* <AddReview/>*/}
+// {/*<Film likeFilms={likeFilms}/>*/}
+// {/*<Error404/>*/}
+// {/*</>*/}
+
 const App = (props) => {
   const {mainFilms, myListFilms, likeFilms} = props;
   return (
-    <>
-      {/* <Main mainFilms = {mainFilms}/>*/}
-      {/* <MyList myListFilms={myListFilms}/>*/}
-      {/* <Player/>*/}
-      {/* <AddReview/>*/}
-      {/*<Film likeFilms={likeFilms}/>*/}
-      <Error404/>
-    </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main mainFilms = {mainFilms} />
+      </Route>
+      <Route exact path="/login">
+        <SignIn />
+      </Route>
+      {/*<Route exact path="/mylist">*/}
+        {/*<MyList myListFilms={myListFilms}/>*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/films/:id">*/}
+        {/*<Film likeFilms={likeFilms}/>*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/films/:id/review">*/}
+        {/*<AddReview />*/}
+      {/*</Route>*/}
+      {/*<Route exact path="/player/:id">*/}
+        {/*<Player />*/}
+      {/*</Route>*/}
+      <Route>
+        <Error404 />
+      </Route>
+    </Switch>
+  </BrowserRouter>
   );
 };
 
