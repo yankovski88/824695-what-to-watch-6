03dade78bf5ec8818ab3ee12b67904e6678a1db5
yura <boets003@mail.ru>
2021-03-22 00:29:11 +0300

    add: commit 10

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {AuthorizationStatus} from '../../constants';
+
+const PrivateRoute = ({render, path, exact, authorizationStatus}) =>{
+  return (
+    <Route
+      path={path} // думаю это путь
+      exact={exact} // не знаю
+      render={(routeProps)=>{
+        return (
+          authorizationStatus === AuthorizationStatus.AUTH // если статус равен AUTH т.е. авторизирован
+          ? render(routeProps) // то рендари это routeProps
+            : <Redirect to={`/login`} /> // иначе типо верни на путь с регистрацией
+        )
+      }}
+      />
+  )
+}
+
+PrivateRoute.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
