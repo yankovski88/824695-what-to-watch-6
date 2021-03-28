5b4c54386d31eb78b5a65d8e5b4fc36c227f5186
yura <boets003@mail.ru>
2021-03-28 16:10:16 +0300

    fix: route block

--- src/components/user-block/user-block.jsx
+++ src/components/user-block/user-block.jsx
@@ -3,15 +3,34 @@ import {fetchAllFavoriteFilms} from "../../store/api-actions";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {ActionCreator} from "../../store/action";
+import {Link, useHistory } from "react-router-dom";
 // import {SignInForm} from "../sign-in-form/sign-in-form";
 
 const UserBlock = (props)=>{
-  const {redirectToMyList} = props; //
+  // const {redirectToMyList} = props; //
+  let history = useHistory();
+
+  function handleClick(evt) {
+    evt.preventDefault()
+    history.push("/mylist");
+  }
+
+  // function handleClickByRedax(evt) {
+  //   evt.preventDefault()
+  //   redirectToMyList()
+  // }
+
   return (
-    //
-    <div className="user-block" onClick={redirectToMyList}>
+
+    <div className="user-block" >
       <div className="user-block__avatar">
-        <a href="#" >
+        {/*to={"/mylist"}*/}
+        <a
+          href="#"
+          // onClick={handleClickByRedax}
+
+          onClick={handleClick}
+        >
           <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
 
 
@@ -22,9 +41,9 @@ const UserBlock = (props)=>{
 };
 
 // export default UserBlock;
-UserBlock.propTypes = {
-  redirectToMyList: PropTypes.func.isRequired,
-};
+// UserBlock.propTypes = {
+//   redirectToMyList: PropTypes.func.isRequired,
+// };
 
 const mapDispatchToProps = (dispatch)=>({
   // loadAllFavoriteFilms() {
@@ -32,9 +51,9 @@ const mapDispatchToProps = (dispatch)=>({
   // },
 
 
-  redirectToMyList(){
-    dispatch(ActionCreator.redirectToRoute(`/mylist`))
-  },
+  // redirectToMyList(){
+  //   dispatch(ActionCreator.redirectToRoute(`/mylist`))
+  // },
 
 });
 
