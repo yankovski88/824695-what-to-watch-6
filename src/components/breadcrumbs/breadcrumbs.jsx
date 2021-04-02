26b7e81a106c74a8da2e5bd82460e6d411b51e49
yura <boets003@mail.ru>
2021-04-02 20:04:37 +0300

    fix: marshrut film id

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import PropTypes from "prop-types";
 
 
 const Breadcrumbs = (props) => {
   const {film} = props;
   const {name} = film;
+  const params = useParams();
   return (
     <nav className="breadcrumbs" >
       <ul className="breadcrumbs__list">
         <li className="breadcrumbs__item">
-          <Link to="/film" className="breadcrumbs__link">{name}</Link>
+          <Link to={`/films/${params.id}`} className="breadcrumbs__link">{name}</Link>
         </li>
         <li className="breadcrumbs__item">
           <a className="breadcrumbs__link">Add review</a>
